ae5b57a8555fa28dfd32d22c507e5a57bf734a40
Matus49 <matus.bucko10@gmail.com>
2026-09-14 12:03:40 +0200

    Clean up main.c by removing comments
    Removed comments and adjusted formatting in the main function.

--- main.c
+++ main.c
@@ -8,31 +8,25 @@ int main(void) {
         printf("Nespravny vstup.\n");
         return 1;
     }
-
     int spaces = 0;
     int stars = rows;
 
     while (stars > 0) {
-        // Vytlačenie medzier na začiatku
         for (int i = 0; i < spaces; i++) {
             printf(" ");
         }
 
-        // Vytlačenie hviezdičiek
         for (int i = 0; i < stars; i++) {
             printf("*");
         }
 
-        // Vytlačenie medzier na konci a nový riadok
         for (int i = 0; i < spaces; i++) {
             printf(" ");
         }
         printf("\n");
-
-        // V každom kroku zmenšíme počet hviezdičiek o 2 a pridáme 1 medzeru
+        
         stars -= 2;
         spaces += 1;
     }
-
     return 0;
 }
