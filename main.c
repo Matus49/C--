703b90a9a80d58cfd1bba28127647982e528fbf8
Matus49 <matus.bucko10@gmail.com>
2026-09-14 12:11:20 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
+// *********Pyramida********* 
+
 #include <stdio.h>
 
 int main(void) {
     int rows;
 
     printf("Daj cislo: ");
+    
     if (scanf("%d", &rows) != 1 || rows <= 0) {
         printf("Nespravny vstup.\n");
         return 1;
     }
+    
     int spaces = 0;
     int stars = rows;
 
@@ -23,13 +27,15 @@ int main(void) {
         for (int i = 0; i < spaces; i++) {
             printf(" ");
         }
+        
         printf("\n");
         
         stars -= 2;
         spaces += 1;
+        
     }
     return 0;
 }
-
+// **************************************************
 
 
